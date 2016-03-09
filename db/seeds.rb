af6b0b645dc1dbcdbad460156f3edfc287ed9e2a
Lisardo <lisardo.kist@gmail.com>
2016-03-09 14:38:24 -0300

    Seed db script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Word.create([{ graphy: 'house', definition: 'where you live', phony: '/haʊs/' }, 
+			{ graphy: 'mouse', definition: 'small animal', phony: '/maʊs/' }])
